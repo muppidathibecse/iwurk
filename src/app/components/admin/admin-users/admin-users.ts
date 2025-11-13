496f068542a0052308bdfbd13fa1b6d838e16aaa
muppidathibecse <muppidathi.m@softsuave.org>
2025-11-13 20:23:47 +0530

    13-11-2025-2

--- src/app/components/admin/admin-users/admin-users.ts
+++ src/app/components/admin/admin-users/admin-users.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { usersIcon } from '../../../icons/admin-navigation-icons/users-icon';
+import { CommonModule } from '@angular/common';
+import { searchIcon } from '../../../icons/admin-navigation-icons/search-icon';
+import { filterIcon } from '../../../icons/filter';
+import { dateIcon } from '../../../icons/date';
 
 @Component({
   selector: 'app-admin-users',
-  imports: [],
+  imports: [usersIcon, CommonModule, searchIcon, filterIcon, dateIcon],
   templateUrl: './admin-users.html',
   styleUrl: './admin-users.css',
 })
 export class AdminUsers {
-
+  usersDashboardItems = [
+    { icon: 'assets/admin/users/users.svg', title: 'Total Usesr' },
+    { icon: 'assets/admin/users/activeusers.png', title: 'Active Users' },
+    { icon: 'assets/admin/users/inactiveusers.png', title: 'Inactive Users' },
+    { icon: 'assets/admin/users/onbench.png', title: 'Onbench' },
+  ];
 }
---
+++ src/app/icons/date.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date-icon',
+  standalone: true,
+  template: `
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M16.5665 4.2998V5.80209M7.55273 4.2998V5.80209"
+        stroke="currentColor"
+        stroke-width="1.13901"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M12.053 12.5625H12.0597M12.053 15.5671H12.0597M15.0541 12.5625H15.0609M9.05176 12.5625H9.0585M9.05176 15.5671H9.0585"
+        stroke="#6B7280"
+        stroke-width="1.51868"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M5.6748 8.80664H18.4442"
+        stroke="currentColor"
+        stroke-width="1.13901"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M4.9209 11.9938C4.9209 8.72082 4.9209 7.08434 5.86142 6.06756C6.80194 5.05078 8.31569 5.05078 11.3432 5.05078H12.7703C15.7978 5.05078 17.3116 5.05078 18.2521 6.06756C19.1926 7.08434 19.1926 8.72082 19.1926 11.9938V12.3795C19.1926 15.6525 19.1926 17.2889 18.2521 18.3058C17.3116 19.3225 15.7978 19.3225 12.7703 19.3225H11.3432C8.31569 19.3225 6.80194 19.3225 5.86142 18.3058C4.9209 17.2889 4.9209 15.6525 4.9209 12.3795V11.9938Z"
+        stroke="currentColor"
+        stroke-width="1.13901"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M5.30176 8.80664H18.8223"
+        stroke="currentColor"
+        stroke-width="1.13901"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  `,
+  styles: [],
+})
+export class dateIcon {}
